c626444b6c7ab85ba45dbe51eb00b94be7a1c9b4
Titus Wormer <tituswormer@gmail.com>
2023-01-07 13:35:14 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {u} from 'unist-builder'
 import {h} from 'hastscript'
 import {classnames} from './index.js'
 
-test('hast-util-classnames', (t) => {
-  t.throws(
+test('hast-util-classnames', () => {
+  assert.throws(
     () => {
       classnames(u('comment', '?'))
     },
@@ -12,33 +13,37 @@ test('hast-util-classnames', (t) => {
     'should ignore a given doctype'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames(h('a'), 'alpha'),
     h('a.alpha'),
     'should support a string'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames(h('a'), 'alpha  bravo charlie'),
     h('a.alpha.bravo.charlie'),
     'should support a string (space-separated)'
   )
 
-  t.deepEqual(classnames(h('a'), 123), h('a.123'), 'should support a number')
+  assert.deepEqual(
+    classnames(h('a'), 123),
+    h('a.123'),
+    'should support a number'
+  )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames(h('a'), null, undefined, 2),
     h('a.2'),
     'should support (ignore) nullish values'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames(h('a'), {alpha: true, bravo: false, charlie: true}),
     h('a.alpha.charlie'),
     'should support an object'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames(h('a'), [
       'alpha',
       {bravo: true, charlie: false},
@@ -48,35 +53,33 @@ test('hast-util-classnames', (t) => {
     'should support an array'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames(h('a'), ['alpha', 'bravo', {alpha: false, charlie: true}]),
     h('a.bravo.charlie'),
     'should dedupe'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames(h('a.alpha.bravo'), ['alpha', {alpha: true}]),
     h('a.alpha.bravo'),
     'should merge w/ current classnames'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames(u('element', {tagName: 'a'}, []), ['alpha']),
     h('a.alpha'),
     'should not fail if an element is missing properties'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames(h('a'), 'alpha', {bravo: true, charlie: false}, ['delta', 123]),
     h('a.123.alpha.bravo.delta'),
     'should support multiple conditionals'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     classnames('alpha', ['bravo', 123], {charlie: true}),
     ['123', 'alpha', 'bravo', 'charlie'],
     'should return a list if w/o element'
   )
-
-  t.end()
 })
